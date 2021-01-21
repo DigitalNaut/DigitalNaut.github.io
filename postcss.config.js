ccadab7e909a7ec6982ced4ab3602628a1e18f8b
Cybernaut <77c828fa@opayq.com>
2021-01-21 05:56:26 -0600

    Update postcss.config.js
    + Added conditional to purge CSS only in production

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,10 @@
 const tailwindcss = require("tailwindcss");
+
 module.exports = {
   plugins: [
     tailwindcss("./tailwind.js"),
     require("autoprefixer"),
-    require("@fullhuman/postcss-purgecss")({
+    process.env.NODE_ENV === "production" && require("@fullhuman/postcss-purgecss")({
       content: ["./src/**/*.js","./src/**/*.ts", "./src/**/*.tsx", "./public/index.html"],
       defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
     }),
