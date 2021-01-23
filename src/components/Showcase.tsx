1da37aa1322cbda5294463f544c1080970876a25
Cybernaut <77c828fa@opayq.com>
2021-01-23 03:14:42 -0600

    Update Showcase.tsx
    + Bigger font for project showcase titles

--- src/components/Showcase.tsx
+++ src/components/Showcase.tsx
@@ -7,7 +7,7 @@ interface ShowcaseProps {
 const Showcase: React.FC<ShowcaseProps> = ({ children, title }) => {
   return (
     <div className="flex flex-col max-h-full overflow-hidden text-base text-white bg-blue-800 shadow-md place-content-start rounded-tr-3xl rounded-bl-3xl">
-      <div className="px-4 mx-auto my-2 text-lg md:my-4 text-shadow-lg">
+      <div className="px-4 mx-auto my-2 text-2xl md:my-4 text-shadow-lg">
         {title}
       </div>
       <div className="flex flex-col p-4 text-gray-900 bg-blue-50">
